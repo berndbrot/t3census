1d9191f7137684609100d156afda10f93bf00ed8
Marcus <marcus#exp2013@t3sec.info>
2013-07-07 00:46:44 +0200

    [FEATURE] Add Gearman worker for reverse IP lookup

---
+++ gearman-worker/php/ReverseIpLookupWorker.php
@@ -0,0 +1,61 @@
+<?php
+
+$dir = dirname(__FILE__);
+$libraryDir = realpath($dir . '/../../library/php');
+$vendorDir = realpath($dir . '/../../vendor');
+
+require_once $libraryDir . '/Api/Bing/BingApi.php';
+require_once $vendorDir . '/autoload.php';
+
+class ReverseIpLookupWorker {
+
+	private $host;
+	private $port;
+	private $accountKey;
+	private $gearmanWorker;
+
+	public function __construct($host = '127.0.0.1', $port = 4730, $accountKey = '') {
+		$this->host = $host;
+		$this->port = $port;
+		$this->accountKey = $accountKey;
+	}
+
+	public function setUp() {
+		$this->gearmanWorker = new GearmanWorker();
+		$this->gearmanWorker->addServer($this->host, $this->port);
+		$this->gearmanWorker->addFunction("ReverseIpLookup", array($this, 'fetchHostnames'));
+	}
+
+	public function fetchHostnames(GearmanJob $job) {
+		$result = FALSE;
+		$ip = $job->workload();
+
+		$bingApi = new BingApi();
+		$bingApi->setAccountKey($this->accountKey)->setEndpoint('https://api.datamarket.azure.com/Bing/Search');
+
+		$results = $bingApi->setQuery('ip:' . $ip)->getResults();
+
+		return json_encode($results);
+	}
+
+	public function run() {
+		$this->setUp();
+		$this->gearmanWorker->setTimeout(5000); //wake up after 5 seconds
+		echo "Starting...\n";
+		while(1) {
+			@$this->gearmanWorker->work();
+			if ($this->gearmanWorker->returnCode() == GEARMAN_TIMEOUT)
+			{
+				//do some other work here
+				continue;
+			}
+			if($this->gearmanWorker->returnCode() != GEARMAN_SUCCESS) {
+				// do some error handling here
+				die("An error occured");
+			}
+
+		}
+	}
+}
+
+?>
---
+++ gearman-worker/php/StartReverseIpLookupWorker.php
@@ -0,0 +1,45 @@
+<?php
+
+require_once 'ReverseIpLookupWorker.php';
+
+declare(ticks = 1); //this is needed to catch signals
+
+//get a new fetcher by default connecting to localhost:4730
+$fetcher = new ReverseIpLookupWorker('127.0.0.1', 4730, '');
+
+$pid = pcntl_fork();
+
+if ($pid == -1)
+{
+	die("could not fork\n");
+} else if ($pid) {
+	// After the bird exit the parent process this closes the console
+	// and let us operate as a daemon
+	exit(0);
+} else {
+	// we are the child now operating the fetcher
+	// catch some signals
+	pcntl_signal(SIGTERM, "sig_handler");
+	pcntl_signal(SIGHUP,  "sig_handler");
+	pcntl_signal(SIGINT,  "sig_handler");
+
+	$fetcher->run();
+}
+
+// the signal handler is very useless here normally it does some
+// cleanup of the daemon before it exits the child
+function sig_handler($signo) {
+	echo "Signal {$signo} received\n";
+	flush();
+	switch($signo) {
+		case SIGTERM:
+			exit(0);
+			break;
+		case SIGHUP:
+			exit(0);
+			break;
+		case SIGINT:
+			exit(0);
+			break;
+	}
+}
