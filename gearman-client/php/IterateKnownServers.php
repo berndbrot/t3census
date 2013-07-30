2a124d9ccb6a4be08ef02f18875d2332bed0ee39
Marcus <marcus#exp2013@t3sec.info>
2013-07-30 22:42:19 +0200

    [FEATURE] Do not persist non-TYPO3 hosts

--- gearman-client/php/IterateKnownServers.php
+++ gearman-client/php/IterateKnownServers.php
@@ -62,6 +62,7 @@ if (is_array($gearmanStatus)) {
 
 					if (is_object($detectionResult)) {
 						if (is_null($detectionResult->port) || is_null($detectionResult->ip))  continue;
+						if (empty($detectionResult->TYPO3))  continue;
 
 						$portId = getPortId($mysqli, $detectionResult->port);
 
